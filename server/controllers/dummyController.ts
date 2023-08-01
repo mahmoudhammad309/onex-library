3cbaa3ffd076d1d7003d7d7ddfc8cb3afab5fda9
mahmoud <mahmoudhammad3009@gmail.com>
2023-08-01 19:01:01 +0300

    server setup related to #1

--- server/controllers/dummyController.ts
+++ server/controllers/dummyController.ts
@@ -1,7 +1,7 @@
 import { Response, Request } from "express";
 
 export default class dummyController {
-  // get All Contacts
+  
   public static async index(req: Request, res: Response) {
     res.status(200).json({ status: 200, data: "dummy route is running right now :)" });
   }
--- server/helpers/errorHandler/errorWrapper.ts
+++ server/helpers/errorHandler/errorWrapper.ts
@@ -1,21 +1,17 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, RequestHandler, Response } from 'express';
+import { NextFunction, Request, RequestHandler, Response } from "express";
 
 const errorWrapper = (controller: RequestHandler) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await controller(req, res, next);
     } catch (error: any) {
-      if (error.name === 'JsonWebTokenError') {
+      if (error.name === "JsonWebTokenError") {
         error.status = 401;
-      } else if (error.name === 'ValidationError') {
+      } else if (error.name === "ValidationError") {
         error.status = 422;
-      } else if (error.name === 'Unauthorized') {
+      } else if (error.name === "Unauthorized") {
         error.status = 401;
-      } else if (error.name === 'CATEGORY NOT FOUND') {
-        error.status = 404;
-      } else if (error.name === 'PRODUCTS NOT FOUND') {
-        error.status = 404;
       } else if (
         error.name ===
         "The email address you entered isn't connected to an account"
