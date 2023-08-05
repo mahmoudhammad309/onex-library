ca6b1e3e3e9842772de5557326e91e22263c5ad1
mahmoud <mahmoudhammad3009@gmail.com>
2023-08-05 22:48:22 +0300

    setup client related to #10

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import vuetify from "./plugins/vuetify";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-createApp(App).use(router).use(vuetify).mount("#app");
+createApp(App).use(router).mount('#app')
