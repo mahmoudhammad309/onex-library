2f0a721d5397e259c71acda6335a48895bc63326
mahmoud <mahmoudhammad3009@gmail.com>
2023-08-05 22:28:02 +0300

    setup client related to #10

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,16 +1,18 @@
 module.exports = {
+  root: true,
   env: {
-    browser: true,
-    es2021: true,
+    node: true,
   },
   extends: [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    'prettier',
-    'prettier/vue',
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
   ],
-  plugins: ['vue', 'prettier'],
-  rules: {
-    'prettier/prettier': 'error',
+  parserOptions: {
+    parser: "@babel/eslint-parser",
   },
-}
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
