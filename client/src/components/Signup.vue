748c8745b30f1b6a4336e0905f91fe84048a3e26
mahmoud <mahmoudhammad3009@gmail.com>
2023-08-07 12:33:43 +0300

    adding first name and last name

--- client/src/components/Signup.vue
+++ client/src/components/Signup.vue
@@ -9,29 +9,31 @@
     >
       <v-container class="inputs-wrapper">
         <v-col class="form-wrapper" align="center" justify="center">
-          <v-col cols="12" md="12">
             <v-card-title class="title">Signup</v-card-title>
             <v-card-text class="message">
               signup now and get full access to our app.
             </v-card-text>
-          </v-col>
-          <v-col cols="12" md="12">
+
             <v-text-field
-              v-model="fullName"
-              :rules="fullNameRules"
-              label="Full Name"
+              v-model="firstName"
+              :rules="firstNameRules"
+              label="First Name"
               required
             ></v-text-field>
-          </v-col>
-          <v-col cols="12" md="12">
+
+            <v-text-field
+              v-model="lastName"
+              :rules="lastNameRules"
+              label="Last Name"
+              required
+            ></v-text-field>
             <v-text-field
               v-model="email"
               :rules="emailRules"
               label="E-mail"
               required
             ></v-text-field>
-          </v-col>
-          <v-col cols="12" md="12">
+
             <v-text-field
               v-model="password"
               :append-icon="showPassword1 ? 'mdi-eye' : 'mdi-eye-off'"
@@ -41,8 +43,6 @@
               label="Password"
               @click:append="showPassword1 = !showPassword1"
             ></v-text-field>
-          </v-col>
-          <v-col cols="12" md="12">
             <v-text-field
               v-model="confirmPassword"
               :append-icon="showPassword2 ? 'mdi-eye' : 'mdi-eye-off'"
@@ -52,10 +52,8 @@
               label="Confirm Password"
               @click:append="showPassword2 = !showPassword2"
             ></v-text-field>
-          </v-col>
-          <v-col cols="14" md="14">
             <v-btn
-              :disabled="!fullName || !email || !password || !confirmPassword"
+              :disabled="!firstName || !lastName|| !email || !password || !confirmPassword"
               class="signup-btn"
               type="submit"
             >
@@ -68,7 +66,6 @@
               </template>
               <template v-else> Signup </template>
             </v-btn>
-          </v-col>
           <v-col cols="14" md="14">
             <p class="have-account">
               Already have an account ?
@@ -90,14 +87,16 @@ export default {
   name: "LoginPage",
   data() {
     return {
-      fullName: "",
+      firstName: "",
+      lastName: "",
       email: "",
       password: "",
       confirmPassword: "",
       valid: false,
       showPassword1: false,
       showPassword2: false,
-      fullNameRules: [(v) => !!v || "Full Name is required"],
+      firstNameRules: [(v) => !!v || "First Name is required"],
+      lastNameRules: [(v) => !!v || "Last Name is required"],
       emailRules: [
         (v) => !!v || "E-mail is required",
         (v) => /.+@.+/.test(v) || "E-mail must be valid",
@@ -122,7 +121,8 @@ export default {
           this.loading = true;
 
           await userAuth.signup({
-            fullName: this.fullName,
+            firstName: this.firstName,
+            lastName: this.lastName,
             email: this.email,
             password: this.password,
             confirmPassword: this.confirmPassword,
@@ -131,7 +131,8 @@ export default {
           this.$router.push({ path: "/profile" });
         }
       } catch (error) {
-        this.errorMessage = error.response.data.msg;
+        console.log(error)
+        this.errorMessage = error.response?.data.msg;
         this.errorSnackbar = true;
       } finally {
         this.loading = false;
